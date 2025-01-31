a28401122537a337b327bfadcffc4c70ff0153e0
Baek-narae <73625123+beak-narea@users.noreply.github.com>
2025-01-31 23:24:38 +0900

    add 주사위세개 file upload

---
+++ 기초코드작성/주사위세개.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+
+	scanf("%d %d %d", &a, &b, &c);
+	int price = 0;
+	if (a == b) {
+		if (b == c) { // 세 수가 같다면
+			price = 10000 + a * 1000;
+		}
+		else { // 두 수가 같다면
+			price = 1000 + a * 10;
+		}
+	}
+	else{
+		if (b == c || a == c) { // 두 수가 같다면
+			price = 1000 + c * 10;
+		}
+		else {
+			int temp;
+			if (a > b && a > c) {
+				temp = a;
+			}
+			else if (b > a && b > c) {
+				temp = b;
+			}
+			else {
+				temp = c;
+			}
+			price = temp * 100;
+		}
+	}
+
+	printf("%d", price);
+
+	return 0;
+}
