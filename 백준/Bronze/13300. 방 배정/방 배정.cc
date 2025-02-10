4f6c4e9ef68a7973a092d1db9cc575d9dd726896
Baek-narae <73625123+beak-narea@users.noreply.github.com>
2025-02-10 14:30:24 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 1112 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int n, k, s, y, count = 0;
+	int arr[2][6] = { 0 };
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &s, &y);
+		arr[s][y-1]++;
+		if (arr[s][y-1] % 2) {
+			count++;
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
