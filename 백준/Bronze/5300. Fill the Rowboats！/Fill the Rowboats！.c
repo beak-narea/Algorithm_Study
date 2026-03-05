6be71fd3c4dce15bfe615e5a40666a4bcae88cac
Baek-narae <73625123+beak-narea@users.noreply.github.com>
2026-03-05 20:46:37 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i);
+        if (i % 6 == 0 || i == n) {
+            printf("Go! ");
+        }
+    }
+
+    return 0;
+}
